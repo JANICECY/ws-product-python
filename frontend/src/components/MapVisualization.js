8ab48be3b2750617e28a984f6e0a6458f40c8dd0
chenying <janicecy1217@gmail.com>
2021-02-16 21:36:00 -0500

    map visualization using google map

--- frontend/src/components/MapVisualization.js
+++ frontend/src/components/MapVisualization.js
@@ -1,93 +1,124 @@
-import React, { useState } from 'react'
-import { PIOs, dailyStats, hourlyStats } from './dummyData'
-import { Table, Input, Button, Space, Card, Collapse } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import Highlighter from 'react-highlight-words';
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { useSelector } from 'react-redux'
+import { FlagTwoTone } from '@ant-design/icons';
+import { Popover, List } from 'antd';
+import CN_tower from '../static/CN_tower.jpg'
+import EQ_works from '../static/EQ_works.jpeg'
+import niagara_falls from '../static/niagara_falls.jpg'
+import vancouver_harbour from '../static/vancouver_harbour.jpg'
+import { debounce } from 'lodash'
 import { formatNum } from '../helper'
-import { geoCentroid } from "d3-geo";
-import allStates from '../Canada_states.json'
-
-import {
-    ComposableMap,
-    Geographies,
-    Geography,
-    ZoomableGroup,
-    Marker
-} from "react-simple-maps";
-const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-
-const markers = PIOs.map(({ name, lat, lon }) => (
-    {
-        markerOffset: -15,
-        name,
-        coordinates: [lon, lat]
-    }
-))
-
-export default function MapVisualization() {
-    return (
-        <div>
-            <ComposableMap
-                projection="geoAlbersUsa"
-            // projectionConfig={{
-            //     rotate: [58, 20, 0],
-            //     scale: 400
-            // }}
-            >
-                <ZoomableGroup zoom={1}>
-                    <Geographies geography={geoUrl}>
-                        {({ geographies }) => (
-                            <>
-                                {
-                                    geographies
-                                        .filter(d => d.properties.REGION_UN === "Americas")
-                                        .map(geo => (
-                                            <Geography
-                                                key={geo.rsmKey}
-                                                geography={geo}
-                                                fill="#EAEAEC"
-                                                stroke="#D6D6DA"
-                                            />
-                                        ))
-                                }
-                                {/* {geographies.map(geo => {
-                                    const centroid = geoCentroid(geo);
-                                    const cur = allStates.find(s => s.val === geo.id);
-                                    return (
-                                        <g key={geo.rsmKey + "-name"}>
-                                            {cur &&
-                                                centroid[0] > -160 &&
-                                                centroid[0] < -67 &&
-                                                <Marker coordinates={centroid}>
-                                                    <text y="2" fontSize={14} textAnchor="middle">
-                                                        {cur.id}
-                                                    </text>
-                                                </Marker>
-                                            }
-                                        </g>
-                                    );
-                                })} */}
-                            </>
-                        )
-
-                        }
-                    </Geographies>
-
-                    {markers.map(({ name, coordinates, markerOffset }) => (
-                        <Marker key={name} coordinates={coordinates}>
-                            <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                            <text
-                                textAnchor="middle"
-                                y={markerOffset}
-                                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                            >
-                                {name}
-                            </text>
-                        </Marker>
-                    ))}
-                </ZoomableGroup>
-            </ComposableMap>
-        </div>
-    )
+const NAME_TO_IMAGE = {
+    'EQ Works': EQ_works,
+    'CN Tower': CN_tower,
+    'Niagara Falls': niagara_falls,
+    'Vancouver Harbour': vancouver_harbour
 }
+
+function MapVisualization() {
+    const { POIs, dailyStats } = useSelector(state => state)
+    const [hoveredItemName, setHoveredItemName] = useState('')
+
+
+    const center = {
+        lat: 56.1304,
+        lng: -106.3468
+    }
+
+
+    const handleMouseOver = debounce((e, id) => {
+        e.preventDefault()
+        e.stopPropagation()
+        setHoveredItemName(id)
+    }, 200)
+
+
+    const handleMouseLeave = debounce((e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        setHoveredItemName('')
+    }, 200)
+
+    const LocationTable = () => {
+        return (
+            <div className='location-table'>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={POIs}
+                    renderItem={item => (
+                        <List.Item
+                            className='location-item'
+                            onMouseEnter={(e) => handleMouseOver(e, item.name)}
+                            onMouseLeave={handleMouseLeave}
+                            onmouse
+                        >
+                            <List.Item.Meta
+                                title={<span>{item.name}</span>}
+                                description={
+                                    <p>
+                                        <span>Latitude: {item.lat}</span><br />
+                                        <span>Longitute: {item.lon}</span>
+                                    </p>
+                                }
+                            />
+                            <img src={NAME_TO_IMAGE[item.name]} style={{ width: 100 }} />
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
+
+    const LocationMarker = ({ name }) => {
+        return (
+            <div className='location-marker'>
+                <Popover
+                    content={
+                        <p>
+                            {
+                                dailyStats.slice(0, 3).map(({ date, clicks, impressions, revenue }) => <p>
+                                    <div><b>{new Date(date).toLocaleDateString()}</b></div>
+                                    <div><b>Clicks:</b> {formatNum(clicks)}</div>
+                                    <div><b>Impressions:</b> {formatNum(impressions)}</div>
+                                    <div><b>Revenue:</b> {formatNum(revenue)}</div>
+                                </p>)
+                            }
+                        </p>
+                    }
+                    title={name}
+                    visible={name === hoveredItemName ? true : false}
+                    onMouseEnter={(e) => handleMouseOver(e, name)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <FlagTwoTone style={{ fontSize: 30, color: 'red' }} />
+                </Popover>
+                <h3>{name}</h3>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className='d-flex'>
+            {/* Important! Always set the container height explicitly */}
+            <div style={{ height: '100vh', width: '70%' }} >
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'Put your own API key here' }}
+                    defaultCenter={center}
+                    defaultZoom={5}
+                >
+                    {
+                        POIs.map(
+                            ({ lat, lon, name }) => <LocationMarker lat={lat} lng={lon} name={name} />
+                        )
+                    }
+                </GoogleMapReact>
+            </div>
+            <LocationTable />
+        </div>
+    );
+}
+
+export default MapVisualization;
